c6df9b907149ca85a7e990dd9e6921af952377a5
YawnJune <rakjune05@gmail.com>
2025-08-11 01:52:55 -0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,23 +4,35 @@ using namespace std;
 
 int main()
 {
-    string phrase, finale;
-    string maj = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    int i, j;
+    string verbe;
 
-    //Saisie de la phraseCamelCase
-    cout << "Veuillez saisir une phrase en CamelCase ici (ex : BonjourToutLeMonde) : " << endl;
-    cin >> phrase;
+    //Saisie d'un verbe
+    cout << "Veuillez saisir un verbe : ";
+    cin >> verbe;
 
-    //Transformation
-    for (i = 0; i < phrase.length(); i++){
-        for (j = 0; j < maj.length(); j++)
-        if (phrase[i] != phrase[i-1] && phrase[i] == maj[j]){
-            finale += ' ';
-        }
-        finale += phrase[i];
+    //Declaration du groupe du verbe
+    //si la terminaison est : -er
+    if (verbe[verbe.length() - 2] == 'e' && verbe[verbe.length() - 1] == 'r'){
+            if (verbe == "aller"){
+                cout << "Ce verbe est au 3eme groupe." << endl;
+            }
+            else {
+                cout << "Ce verbe est au 1er groupe." << endl;
+            }
     }
-    cout << "Phrase finale : " << finale << endl;
+
+    //si la terminaison est : -ir
+    else if (verbe[verbe.length() - 2] == 'i' && verbe[verbe.length() - 1] == 'r'){
+            cout << "Ce verbe est au 2eme groupe." << endl;
+    }
+
+    //si la terminaison est autre que ça
+    else if (verbe[verbe.length() - 2] == 'r' && verbe[verbe.length() - 1] == 'e'){
+        cout << "Ce verbe est au 3eme groupe." << endl;
+    }
+    else {
+        cout << "Ceci n'est pas un verbe." << endl;
+    }
 
     return 0;
 }
